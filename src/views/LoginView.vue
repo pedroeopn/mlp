24b78ba2ef2ab8a2dc7f8375dfac25169acbb2cb
Pedro Ernestogit config --global user.name pedroeopn <pedro.eopnunes@gmail.com>
2026-03-02 02:25:52 -0300

    fix: corrige estrutura de pastas duplicadas

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -23,7 +23,8 @@ const handleLogin = async () => {
       <div class="text-center mb-10">
         <div class="inline-flex items-center justify-center w-16 h-16 rounded-2xl bg-primary-600 text-white shadow-xl shadow-primary-600/20 mb-4 transform hover:rotate-12 transition-transform duration-300">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="blue">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 0
+            02-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
           </svg>
         </div>
         <h1 class="text-3xl font-bold text-slate-900 tracking-tight">CuidarBem</h1>
